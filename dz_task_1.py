d182e0fe2bc481cd981c593cbff69986e7f208ab
WhiteMirac1e <115178943+WhiteMirac1e@users.noreply.github.com>
2023-09-22 14:20:46 +0300

    Update dz_task_1.py

--- dz_task_1.py
+++ dz_task_1.py
@@ -3,25 +3,40 @@ import json
 import csv
 import pickle
 
+def get_size(path):
+    total = 0
+    for dirpath, dirnames, filenames in os.walk(path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total += os.path.getsize(fp)
+    return total
 
-def write_json_csv_pickle(directory):
-    # write_JSON
-    result_dict_json = {}
-    for dir_path, dir_file, file_name in os.walk(directory):
-        result_dict_json[f'DIRECTORY - {dir_path}'] = [
-            f'FILE - {i} = {os.path.getsize(os.path.abspath(dir_path + "/" + i))} byte' for i in file_name]
-    with open('json_file.json', 'w', encoding='utf-8') as json_file:
-        json.dump(result_dict_json, json_file, indent=4, separators=(',', ':'))
-    # write_CSV
-    data = [["Dir", "Files"]]
-    for key, value in result_dict_json.items():
-        data.append([key, value])
-    with open('csv_file.csv', 'w', encoding='utf-8') as csv_f:
-        write_csv = csv.writer(csv_f, dialect='excel-tab', delimiter=',')
-        write_csv.writerows(data)
-    # write PICKLE
-    with open('pickle_file.bin', 'wb') as pickle_file:
-        pickle.dump(result_dict_json, pickle_file)
+def directory_walker(dir_path):
+    results = []
+    for root, dirs, files in os.walk(dir_path):
+        for name in files:
+            full_path = os.path.join(root, name)
+            results.append({"parent_directory": root, 
+                            "is_file": True,
+                            "name": name,
+                            "size_in_bytes": os.path.getsize(full_path)})
 
+        for name in dirs:
+            full_path = os.path.join(root, name)
+            results.append({"parent_directory": root, 
+                            "is_file": False,
+                            "name": name,
+                            "size_in_bytes": get_size(full_path)})
 
-write_json_csv_pickle(directory='directory')
+    with open("output.json", "w") as json_file:
+        json.dump(results, json_file)
+
+    with open("output.csv", "w") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=results[0].keys())
+        writer.writeheader()
+        writer.writerows(results)
+
+    with open("output.pickle", "wb") as pickle_file:
+        pickle.dump(results, pickle_file)
+
+directory_walker("./my_folder")
